dabdd18a8848b1af262504db99c84f35cd080c79
MomoFlora <2519840456@qq.com>
2026-04-20 17:44:38 +0800

    Create diy-part1.sh for custom build setup
    Add custom build configuration script for Philips HY3000.

---
+++ philips-hy3000/diy-part1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# ===============================================================================
+# Custom Build Configuration
+# ------------------------------------------------------------------------------
+# Project   : ImmortalWrt for MT7981 (Philips HY3000)
+# Repository: https://github.com/MomoFlora/immortalwrt-mt7981-philips-hy3000
+# Maintainer: Zhao Wenwen
+#
+# Description:
+#   Customized build pipeline based on P3TERX Actions-OpenWrt,
+#   tailored for Philips HY3000 platform with MT7981 SoC.
+# ===============================================================================
+
+# 添加信息源
+#echo '#src-git qmodem https://github.com/FUjr/modem_feeds.git' >>feeds.conf.default
